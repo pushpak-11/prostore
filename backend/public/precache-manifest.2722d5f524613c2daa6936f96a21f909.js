8ca1f587d9db83fb78f2dac2531c1867fcac65bd
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2022-03-25 00:26:52 +0530

    initial commit

---
+++ backend/public/precache-manifest.2722d5f524613c2daa6936f96a21f909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae853941bf6929d5b8593840c43c1cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e5f66e71e1cca2c8a9",
+    "url": "/static/css/main.9a9a9c14.chunk.css"
+  },
+  {
+    "revision": "c02812b2e2d40c605151",
+    "url": "/static/js/2.d8e14eb3.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.d8e14eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7e79aff4a306270ff1",
+    "url": "/static/js/3.56e8fa58.chunk.js"
+  },
+  {
+    "revision": "e5e5f66e71e1cca2c8a9",
+    "url": "/static/js/main.4e06d43b.chunk.js"
+  },
+  {
+    "revision": "b9696a40bc8dde540ab9",
+    "url": "/static/js/runtime-main.814d4aac.js"
+  }
+]);
--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json())
-
+app.use(express.static('public'))
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
